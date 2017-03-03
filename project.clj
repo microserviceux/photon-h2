4a92fd2edc207c205b27b362bf4cc673121f963e
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-03-03 01:13:27 +0100

    Upgrade to new version of photon-db

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject tranchis/photon-h2 "0.10.0"
+(defproject tranchis/photon-h2 "0.10.1"
   :description "H2 plugin for the Photon eventstore"
   :url "https://github.com/microserviceux/photon-h2"
   :license {:name "GNU Affero General Public License Version 3"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [korma "0.4.3"]
-                 [tranchis/photon-db "0.10.0"]
+                 [tranchis/photon-db "0.10.1"]
                  [com.h2database/h2 "1.4.193"]
                  [com.taoensso/nippy "2.13.0"]
                  [midje "1.8.3"]]
